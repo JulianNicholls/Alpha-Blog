6c1837f82dd7c316baa8e21d0ba7d2bde4820e11
Julian Nicholls <juliannicholls29@gmail.com>
2015-10-29 10:34:37 +0000

    Add article migrations and basic model

---
+++ db/migrate/20151029102842_add_description_to_article.rb
@@ -0,0 +1,8 @@
+class AddDescriptionToArticle < ActiveRecord::Migration
+  def change
+    add_column :articles, :description, :text
+
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
