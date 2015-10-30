c4b7cf9b6040361f4d3bf62a83b30e0ad15e4416
Julian Nicholls <juliannicholls29@gmail.com>
2015-10-30 17:12:38 +0000

    Add association between User and Articles.
    For now, every article becomes owned by the first user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 
+  before_save { self.email.downcase! }
+
+  has_many :articles
+
   validates :username, presence: true,
             length:{ in: 4..25 },
             uniqueness: { case_sensitive: false }
@@ -8,7 +12,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :email, presence: true,
-            length: { maximum: 255 }
+            length: { maximum: 255 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL }
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030163218) do
+ActiveRecord::Schema.define(version: 20151030165726) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
