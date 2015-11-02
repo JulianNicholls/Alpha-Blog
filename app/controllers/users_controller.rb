a23a3decf117370b612fc30e9e1e9f15acc52d2b
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-02 15:40:36 +0000

    Add users listing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
