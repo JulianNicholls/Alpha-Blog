94c1543b36b86d2bba496dbaea03f234067ab768
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-02 14:38:11 +0000

    Add user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, except: [:new]
   resources :articles
 
   get 'home', to:  'pages#home'
   get 'about', to: 'pages#about'
+
+  get 'signup', to: 'users#new'
 
   root 'pages#home'
 end
