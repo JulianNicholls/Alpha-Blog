11bfcb2b307721148b8765e1f0dbdde7c700d213
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-02 14:52:14 +0000

    Add user edit, with forms as a partial.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -7,10 +9,23 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      flash[:success] ="Welcome to the Alpha Blog #{user.username}."
+      flash[:success] ="Welcome to the Alpha Blog #{@user.username}."
       redirect_to articles_path
     else
       render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update user_params
+      flash[:success] = 'Your account was updated successfully.'
+      redirect_to articles_path
+    else
+      render :edit
     end
   end
 
@@ -18,5 +33,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find params[:id]
   end
 end
