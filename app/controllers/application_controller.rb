9f69a4067e2e461c8752515d5bed95224678e17f
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-03 11:09:29 +0000

    Add current_user? to Application Controller.
    Article partial only adds actions for the article owner.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :current_user?, :logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user?(user)
+    current_user == user
   end
 
   def logged_in?
