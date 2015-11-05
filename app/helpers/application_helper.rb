53c2908c3b5994acca90a1fef00db8fb710a1e31
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-05 10:34:40 +0000

    Correct gravatar_for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   # :reek:FeatureEnvy
   def gravatar_for(user, options = { size: 80 })
     g_id = Digest::MD5.hexdigest user.email
-    g_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
+    g_url = "https://secure.gravatar.com/avatar/#{g_id}?s=#{options[:size]}"
 
     image_tag(g_url, alt: user.username, class: 'img-circle gravatar')
   end
