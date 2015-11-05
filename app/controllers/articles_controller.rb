ea9ac264d28e58f10ff79b0b0b7b2c8469caabab
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-05 12:20:44 +0000

    Add categories checkboxes to create new article.
    The categories are saved along with the article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,7 +52,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def set_article
