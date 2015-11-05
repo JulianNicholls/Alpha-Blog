d3c17f83c449117a5975f0f79dd4150319f9e1c9
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-05 09:56:53 +0000

    Add resources for Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, except: [:new]
   resources :articles
+  resources :categories, except: [:destroy]
 
   get 'home',  to: 'pages#home'
   get 'about', to: 'pages#about'
