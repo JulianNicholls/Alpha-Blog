25c26643421c4ea5bdc16263739c855a0663c285
Julian Nicholls <juliannicholls29@gmail.com>
2015-11-06 16:27:53 +0000

    Add tests for list, show for article, category.

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -3,11 +3,20 @@ require 'test_helper'
 # Integration test for the category list
 class ListCategoriesTest < ActionDispatch::IntegrationTest
   def setup
+    admin = User.create! username: 'admin', email: 'admin@example.com',
+                         password: 'foobar', admin: true
+
+    @article = Article.create! title: 'Test Article with distinctive title',
+                               description: 'Text with a noticeable word',
+                               user: admin
+
     @cat_s = Category.create name: 'sports'
     @cat_p = Category.create name: 'programming'
+
+    @article.categories << @cat_p << @cat_s
   end
 
-  test 'get category list and ensure expected ones are present' do
+  test 'get category list, expect two above, expect article count' do
     get categories_path
     assert_template 'categories/index'
 
@@ -15,9 +24,11 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', category_path(@cat_p), text: @cat_p.name
 
     assert_select 'ul.pagination', count: 0
+
+    assert_match '1 article', response.body, count: 2
   end
 
-  test 'get category list and ensure that the pagination link is present' do
+  test 'get category list, ensure that the pagination link is present' do
     12.times do |idx|
       Category.create name: "Cat #{idx}"
     end
@@ -26,5 +37,16 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
 
     assert_select 'ul.pagination', count: 2
+  end
+
+  test 'get category to show and expect article above to appear' do
+    get category_path(@cat_s)
+
+    assert_template 'categories/show'
+
+    assert_select 'a[href=?]', article_path(@article), text: @article.title
+
+    get category_path(@cat_p)
+    assert_select 'a[href=?]', article_path(@article), text: @article.title
   end
 end
